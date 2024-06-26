3f8b4a7109470c354886f2535d82ede0ce54e3bf
ramaeb <bianucciramiro@hotmail.com>
2024-06-26 12:24:52 -0300

    deleted:    get-pip.py 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+
+pygame.init() #Inicialización de pygame
+screen = pygame.display.set_mode((800,600)) #Seteo del tamaño de la pantalla
+pygame.display.set_caption("HOLA")#Seteo del nombre de la ventana.
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pass

4aa040078c74d08c659a03bf3a567ca4142e17b4
ramaeb <bianucciramiro@hotmail.com>
2024-06-26 18:42:06 -0300

    new file:   __pycache__/constantes.cpython-311.pyc 	new file:   constantes.py 	new file:   img/preguntados_background.jpg 	modified:   main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
 import sys
-
+from constantes import *
 pygame.init() #Inicialización de pygame
-screen = pygame.display.set_mode((800,600)) #Seteo del tamaño de la pantalla
-pygame.display.set_caption("HOLA")#Seteo del nombre de la ventana.
-
+screen = pygame.display.set_mode((HEIGHT,WIDTH)) #Seteo del tamaño de la pantalla
+pygame.display.set_caption("Preguntados!")#Seteo del nombre de la ventana.
+imagen_fondo = pygame.image.load('img/preguntados_background.jpg')
+imagen_fondo = pygame.transform.scale(imagen_fondo, (HEIGHT,WIDTH))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
             #HOLAasasas
+    screen.blit(imagen_fondo,(0,0))
+    pygame.display.update()
     pass

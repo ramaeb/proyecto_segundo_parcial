903b20f969bacd63b49406e5836686f3676df6ad
ramaeb <bianucciramiro@hotmail.com>
2024-06-30 21:27:18 -0300

    new file:   __pycache__/escenas.cpython-311.pyc 	modified:   __pycache__/funciones.cpython-311.pyc 	modified:   __pycache__/main.cpython-311.pyc 	new file:   clases.py 	new file:   escenas.py 	modified:   funciones.py 	modified:   main.py 	renamed:    img/preguntados_background.jpg -> mult/img/preguntados_background.jpg 	renamed:    img/rueda_configuracion.png -> mult/img/rueda_configuracion.png 	new file:   mult/img/sonido/inicio_efecto.mp3 	new file:   mult/img/sonido/musica_fondo.mp3

--- funciones.py
+++ funciones.py
@@ -2,10 +2,19 @@ import pygame
 import sys
 from pygame import *
 from constantes import *
-
-
-def crea_boton(izquierda,arriba,ancho,alto,texto,fuente_nombre="Calibri",color=(255,255,255)):
-    '''
+import json
+import csv
+import datetime
+import random
+ruta_preguntas = "proyecto_segundo_parcial\datos\preguntas.csv"
+def crea_texto(texto:str,fuente_nombre="Calibri"):
+    fuente = font.SysFont(fuente_nombre,40)#Seteo de fuente
+    text_surface = fuente.render(texto, True, (255,255,255))#
+    text_rect = text_surface.get_rect()
+    return text_rect,text_surface                
+                                                                        
+def crea_boton(izquierda:int,arriba:int,ancho:int,alto:int,texto:str,fuente_nombre="Calibri",color=(255,255,255)):
+    '''                     
     Retorna BOTON INICIO, RECT DEL TEXT, Y LA SUPERFICIE DEL TEXTO
     '''
     fuente = font.SysFont(fuente_nombre,40)#Seteo de fuente
@@ -25,3 +34,104 @@ def colision_boton(evento,boton):
     
     if(mouse_pos[0]> boton.left and mouse_pos[0]<boton.right) and (mouse_pos[1]< boton.bottom and mouse_pos[1]> boton.top):
         return True
+    
+
+    
+# LISTA JUGADORES TOP 10
+def puntaje_jugadores(lista_jugadores):
+        for i in range(len(lista_jugadores)):
+            for j in range(i + 1,len(lista_jugadores)):
+                if lista_jugadores[i]['puntaje'] < lista_jugadores[j]['puntaje']:
+                    aux = lista_jugadores[i]
+                    lista_jugadores[i] = lista_jugadores[j]
+                    lista_jugadores[j] = aux
+        print(lista_jugadores)
+
+
+#FUNCIONES EN REVISION
+def iniciar_juego(datos):
+    '''
+    No usar, en revision
+    '''
+    contador_fallas = 0
+    puntaje = 0
+    datos_jugadores = []
+
+    for i in range(len(datos)):
+
+        datos_jugador = {}
+
+        if contador_fallas == 3: # si pierde 3 vidas se termina el juego 
+            print('perdiste pete')  
+            nombre = input('Ingresa tu nombre: ')   # pide nombre
+            fecha_actual = datetime.datetime.now()  # pide fecha
+            fecha_actual = str(fecha_actual)        # pasa fecha a str (para guardar en json)
+            datos_jugador['nombre'] = nombre        # agrega valores al diccionario
+            datos_jugador['puntaje'] = puntaje
+            datos_jugador['fecha'] = fecha_actual          
+            datos_jugadores.append(datos_jugador)   # agrega diccionario a lista 
+            break
+
+        print(datos[i]['pregunta'])             # printea preguntas y posibles respuestas
+        print(datos[i]['respuesta_a'])
+        print(datos[i]['respuesta_b'])
+        print(datos[i]['respuesta_c'])
+
+        respuesta = input('Ingrese respuesta: ') # opcion 
+
+
+        while respuesta != datos[i]['respuesta_correcta']: # si responde mal
+
+            contador_fallas += 1 # resta una vida
+
+            if contador_fallas == 3: # al perder 3 vidas rompe
+                break
+
+            print('respuesta incorrecta')
+            respuesta = input('Reingrese respuesta: ') # vuelve a preguntar 
+
+            if respuesta == datos[i]['respuesta_correcta']: # si la respuesta es correcta suma punto
+                puntaje += 1
+
+
+def leer_csv(ruta_preguntas):
+    '''
+    No usar, en revision
+    '''
+    datos = []
+    with open(ruta_preguntas, mode = 'r') as arhivo:
+        lectura = csv.DictReader(arhivo)
+
+        for fila in lectura:
+            datos.append(dict(fila))
+
+    try:
+        with open('jugadores.json', 'r') as archivo:
+            datos_jugadores = json.load(archivo)
+    except FileNotFoundError:
+        # Si el archivo no existe, se crea una lista vacía
+        datos_jugadores = []
+    return datos
+def preguntas():
+    '''
+    No usar, en revision
+    '''
+    while True:
+        print('-PREGUNTADOS-')
+        print('1 - Iniciar')
+        print('2 - TOP 10 PARTIDAS')
+        print('3 - Fin')
+        opcion = int(input('Ingrese opcion: '))
+
+        if opcion == 1:
+            iniciar_juego(datos)
+
+        elif opcion == 2:
+            puntaje_jugadores(datos_jugadores)
+        elif opcion == 3:
+            with open(ruta_datos_json, 'w') as archivo:
+                json.dump(datos_jugadores,archivo)
+            break
+            
+
+print(leer_csv(ruta_preguntas))
--- main.py
+++ main.py
@@ -3,53 +3,75 @@ import sys
 from funciones import *
 from pygame import *
 from constantes import *
-
+from escenas import *
 #VARIABLES GLOBALES
 escena = "Menu"
-andando = True
 
-def menu_principal():
+
+def menu_principal(screen):
     '''
     Escena de menu principal, crea dos botones.
     MISION: MOVER ESCENAS A UN ARCHIVO CON FUNCIONES PARA NO TENERLO ACÁ
     '''
-    global andando
     global escena
-    boton_inicio,text_rect,text_surface = crea_boton(200,500,100,200,"hola")
-    boton_inicio_2,text_rect_2,text_surface_2 = crea_boton(150,300,100,200,"Inicio2")
+    boton_inicio,text_rect,text_surface = crea_boton(300,370,200,70,"Inicio")
+    boton_salida,text_rect_2,text_surface_2 = crea_boton(300,500,200,70,"Salida")
+    sonido_inicio = pygame.mixer.Sound("proyecto_segundo_parcial/mult/img/sonido/inicio_efecto.mp3")
+    sonido_inicio.set_volume(0.2)
+    sonido_inicio.play()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.MOUSEBUTTONUP:
+        elif (event.type == pygame.MOUSEBUTTONUP):
             if event.button == 1:
                 mouse_pos = event.pos
                 print(mouse_pos)
-                if(mouse_pos[0]> boton_inicio.left and mouse_pos[0]<boton_inicio.right) and (mouse_pos[1]< boton_inicio.bottom and mouse_pos[1]> boton_inicio.top):
-                    print("Toque Boton...")
-                    escena = "Inicio"
-                colision_boton(event,boton_inicio)
-
+                if colision_boton(event,boton_inicio):
+                        print("Toque Boton...")
+                        escena = "Inicio"
+                elif (mouse_pos[0]> boton_salida.left and mouse_pos[0]<boton_salida.right) and (mouse_pos[1]< boton_salida.bottom and mouse_pos[1]> boton_salida.top):
+                    pygame.quit()
+                    sys.exit()
+                
     screen.blit(imagen_fondo,(0,0))
-    draw.rect(screen,VERDE,boton_inicio,0)#Boton inicio
-    draw.rect(screen,AZUL,boton_inicio_2,0)#Boton 2
+    draw.rect(screen,(116, 191, 64),boton_inicio,0,10,10)#Boton inicio
+    draw.rect(screen,(248, 41, 51),boton_salida,0,10,10)#Boton salida (despues del 0 parametros para redondeo del boton)
     screen.blit(text_surface_2, text_rect_2)
-    screen.blit(text_surface, text_rect)   
+    screen.blit(text_surface, text_rect)    
 
 
+def inicio(screen):
+    '''
+    musica_fondo = pygame.mixer.Sound("proyecto_segundo_parcial\mult\img\sonido\musica_fondo.mp3")
+    musica_fondo.set_volume(0.2)
+    musica_fondo.play()
+    '''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill((253, 87, 34))
+    text_rect,text_surface = crea_texto("De que color es ?")
+    screen.blit(text_surface,text_rect)
+    print("Estoy en inicio")
+
+
+'''
+Inicio pygame
+'''
 pygame.init() #Inicialización de pygame
 screen = pygame.display.set_mode((HEIGHT,WIDTH)) #Seteo del tamaño de la pantalla
 pygame.display.set_caption("Preguntados!")#Seteo del nombre de la ventana.
-imagen_fondo = pygame.image.load('proyecto_segundo_parcial/img/preguntados_background.jpg')
+imagen_fondo = pygame.image.load('proyecto_segundo_parcial/mult/img/preguntados_background.jpg')
 imagen_fondo = pygame.transform.scale(imagen_fondo, (HEIGHT,WIDTH))
-
-
 while True:
     match escena:
         case "Menu":
-            menu_principal()
+            menu_principal(screen)
         case "Inicio":
-            print("HOLA")
+            inicio(screen)
 
     display.flip()
     pygame.display.update()
